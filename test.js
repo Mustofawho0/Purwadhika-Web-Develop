7e77e8d5b81e39c10b367441dfb079c222af7853
Mustofawho0 <mustofawho12@gmail.com>
2024-02-09 16:18:12 +0700

    udah dimerge 3x

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
-console.log('Mustofa')
-console.log('who')
-console.log('Hi ini udah di merge')
-let a = 1
-console.log('ini udah di merge 2x')
+console.log("Mustofa");
+console.log("who");
+console.log("Hi ini udah di merge");
+let a = 1;
+console.log("ini udah di merge 2x");
+console.log('ini udah di merge 3x')
