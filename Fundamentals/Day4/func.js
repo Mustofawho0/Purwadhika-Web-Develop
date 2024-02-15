1d582bb38f405b6a5fff4681a36534aeb7cfcdad
Mustofawho0 <mustofawho12@gmail.com>
2024-02-15 19:57:03 +0700

    js

--- Fundamentals/Day4/func.js
+++ Fundamentals/Day4/func.js
@@ -1,34 +1,114 @@
 // FUNCTION
 
 // 1. Build
-function lopNum (){
-    const n = 10
-    for (i = 1; i <= n; i++){
-        console.log(i)
-    }
+function lopNum() {
+  const n = 10;
+  for (i = 1; i <= n; i++) {
+    console.log(i);
+  }
 }
-lopNum()
+lopNum();
 
 // 2. Call
-lopNum()
-lopNum()
-lopNum()
+lopNum();
+lopNum();
+lopNum();
 
 // FUNCTION TYPE :
 // - Function Declaraction
-print()
-function print(){
-    console.log('Hello World 1')
+print();
+function print() {
+  console.log("Hello World 1");
 }
 
 // - Function Expression
-let output = function(){
-    console.log('Hello World 2')
-}
-output()
+let output = function () {
+  console.log("Hello World 2");
+};
+output();
 
 // - Arrow Function
 let result = () => {
-    console.log('Hello World 3')
+  console.log("Hello World 3");
+};
+result();
+
+// FUNCTION with PARAMETER
+
+function loop(totalArgument){ // : Parameter
+    for (i = 1; i <= totalArgument; i++){
+        console.log(i)
+    }
 }
-result()
+loop(10) // : Argument
+
+function profile(nama, hobi, alamat){ // Lebih dari 1 para meter
+    console.log(`Nama saya ${nama} , Hobi saya ${hobi} , Alamat saya ${alamat}`)
+}
+profile('Mustofa', 'Bengong', 'TNG') // Urutan dalam pengisian argument harus mengikuti variable / nanti akan menjadi acak
+
+// FUNCTION with RETURN
+
+function Penjumlahan(numPlus, numPlus1) {
+    return numPlus + numPlus1;
+  }
+  function Pengurangan(numMin, numMin1) {
+    return numMin - numMin1;
+  }
+  
+  function Calculator(num, num1, operator) {
+    if (operator === "+") {
+      return Penjumlahan(num, num1);
+    } else if (operator === "-") {
+      return Pengurangan(num, num1);
+    }
+  }
+  console.log(Calculator(5, 5, "+"));
+  
+  function Calculator(num2, num3, operator1){
+      if(operator1 === '+'){
+          console.log(Penjumlahan(num2,num3))
+      }else if(operator1 === '-'){
+          console.log(Pengurangan(num2,num3))
+      }
+  }
+  Calculator(4,5,'-')
+
+  // FUNCTION with DEFAULT PARAMETER
+
+function PrintOut(name = 'Anonymous'){
+    return `Hello my Name ${name}`
+}
+
+console.log(PrintOut('Mustofa'))
+
+// FUNCTION with REST PARAMETER
+
+function OutPut (a,b,c,d,...Bebas){
+    console.log(a)
+    console.log(b)
+    console.log(c)
+    console.log(d)
+    console.log(Bebas)
+}
+OutPut(1,2,3,4,5,6,7,8,9,10)
+
+// CLOSURE FUNCTION : Inner Function yang dapat mengakses Variable di Outer Function
+
+function free(name){
+    function free1 (){
+        return `My Name is ${name}`
+    }
+}
+
+// RECURSIVE FUNCTION
+// Function Perulangan
+
+function Rec(num){
+    console.log(num)
+    if(num < 5){
+        Rec(num+1)
+    }
+}
+
+Rec(0)
---
+++ Fundamentals/Day5/Exe.js
@@ -0,0 +1,82 @@
+/*
+1. Write a function to get the lowest, highest and average value in the array (with and without sort function).
+a. Example : 
+arr = [12, 5, 23, 18, 4, 45, 32] → {lowest : 4, highest: 45, average: 19.8xxx}
+*/
+function LowHighAve(arrNumber) {
+  let low = arrNumber[0];
+  let high = arrNumber[1];
+  let ave = 0;
+
+  for (let item of arrNumber) {
+    if (item < low) low = item;
+    if (item > high) high = item;
+    ave += item;
+  }
+  return [low, high, ave / arrNumber.length];
+}
+
+console.log(LowHighAve([12, 5, 23, 18, 4, 45, 32]));
+
+/*
+    2. Write a function that takes an array of words and returns a string by concatenating the words in the array,
+    separated by commas and - the last word - by an 'and'.
+        a. Example : 
+        arr = ["apple", "banana", "cherry", "date"] → “apple,banana,cherry, and date”
+*/
+
+function ComaAnd(arrStr) {
+  return (
+    arrStr.slice(0, arrStr.length - 1).join(" , ") +
+    ", and " +
+    arrStr[arrStr.length - 1]
+  );
+}
+console.log(ComaAnd(["Apple", "Banana", "Cherry", "Date"]));
+
+/*
+    3. Write a function to split a string and convert it into an array of words
+        a. Example : “Hello World” → [“Hello”, “World”]
+*/
+function huruf(txt) {
+  console.log(txt.split(" "));
+}
+huruf("Hello World");
+
+/*
+    4. Write a function to calculate each element in the same position from two arrays of integer. Assume both arrays are
+    of the same length.
+        a. Example : 
+        [1, 2, 3] + [3, 2, 1] → [4, 4, 4]
+
+*/
+function ArrLength(arr1, arr2) {
+  let arrTot = [];
+  for (i = 0; i < arr1.length; i++) {
+    arrTot.push(arr1[i] + arr2[i]);
+  }
+  return arrTot;
+}
+
+console.log(ArrLength([1, 2, 3], [3, 2, 1]));
+
+/*
+    5. Write a function that adds an element to the end of an array. However, the element should only be added if it is
+    not already in the array.
+        a. Example : 
+                arr = [1, 2, 3, 4], 
+                newElement = 4 → [1, 2, 3, 4]
+        b. Example : 
+                arr = [1, 2, 3, 4], 
+                newElement = 7 → [1, 2, 3, 4, 7]
+*/
+
+function AddElementArr(arr, newArr) {
+  if (!arr.includes(newArr)) {
+    // " i " : Negasi
+    arr.push(newArr);
+  }
+  return arr;
+}
+
+console.log(AddElementArr([1, 2, 3, 4], 4));
