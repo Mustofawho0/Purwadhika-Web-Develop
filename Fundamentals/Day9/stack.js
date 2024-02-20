2ed66ccb36d29f1a53fd87e3005ddf6938d056c9
Mustofawho0 <mustofawho12@gmail.com>
2024-02-20 13:51:00 +0700

    js

--- Fundamentals/Day9/stack.js
+++ Fundamentals/Day9/stack.js
@@ -80,5 +80,149 @@ newCamp.forEach((item) => {
 
 campus.forEach((index, item) => {
   console.log(index);
-  console.log(item)
+  console.log(item);
 });
+
+// LINKED LIST
+// SINGLE LINKED
+const list = {
+  head: {
+    value: 1,
+    next: {
+      value: 2,
+      next: {
+        value: 3,
+        next: {
+          value: 4,
+          next: null,
+        },
+      },
+    },
+  },
+};
+
+console.log(list.head.next);
+
+//  Linked List Implementation
+
+class Node {
+  constructor(element) {
+    this.element = element;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+  // ADD ELEMENT
+  add(element) {
+    // Create new node
+    let node = new Node(element);
+    let current;
+
+    if (this.head == null) {
+      this.head = node;
+    } else {
+      current = this.head;
+      // iterate to the end of the list
+      // note : end of the list -> next === null
+      while (current.next) {
+        current = current.next;
+      }
+      // add node
+      current.next = node;
+    }
+    this.size += 1;
+  }
+  // Method printList in LinkedList
+  printList() {
+    var curr = this.head;
+    while (curr) {
+      console.log(curr.element);
+      curr = curr.next;
+    }
+  }
+  // Method insertAt in LinkedList
+  insertAt(element, index) {
+    if (index < 0 || index > this.size)
+      return console.log("Please Enter a valid index");
+    else {
+      const node = new Node(element);
+      let curr = this.head;
+
+      if (index == 0) {
+        node.next = this.head;
+        this.head = node;
+      } else {
+        curr = this.head;
+        let prev;
+        let it = 0;
+
+        while (it < index) {
+          it++;
+        }
+        for (let i = 0; i < index; i++) {
+          prev = curr;
+          curr = curr.next;
+        }
+        prev.next = node;
+        node.next = curr;
+      }
+      this.size += 1;
+    }
+  }
+  // Method removeAt in LinkedList
+  removeAt(index) {
+    if (index < 0 || index >= this.size)
+      return console.log("Please Enter a valid index");
+    else {
+      let curr = this.head;
+      let prev = curr;
+
+      if (index === 0) {
+        this.head = curr.next;
+      } else {
+        for (let i = 0; i < index; i++) {
+          prev = curr;
+          curr = curr.next;
+        }
+        prev.next = curr.next;
+      }
+      this.size -= 1;
+      return curr.element;
+    }
+  }
+  removeElement(element) {
+    let current = this.head;
+    let prev = null;
+
+    while (current != null) {
+      if (current.element === element) {
+        if (prev == null) {
+          this.head = current.next;
+        } else {
+          prev.next = current.next;
+        }
+        this.size -= 1;
+        return current.element;
+      }
+      prev = current;
+      current = current.next;
+    }
+    return null;
+  }
+}
+
+const linkedList = new LinkedList();
+linkedList.add("A");
+linkedList.add("B");
+linkedList.add("C");
+linkedList.add("D");
+linkedList.insertAt("NewValue", 2);
+linkedList.removeElement("B");
+linkedList.removeAt(3);
+
+linkedList.printList();
