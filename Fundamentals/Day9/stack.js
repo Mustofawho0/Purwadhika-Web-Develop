32d4e6f2496f70b1c284c7816b95b36b03352dba
Mustofawho0 <mustofawho12@gmail.com>
2024-02-20 10:17:46 +0700

    js

--- Fundamentals/Day9/stack.js
+++ Fundamentals/Day9/stack.js
@@ -1,25 +1,84 @@
 // STACK
 // LIFO (Last In - First Out)
 
-class Stack {
-    maxSize  = 3
-    container = []
+class LIFOStack {
+  maxSize = 3;
+  container = [];
 
-    push(data){
-        if(this.container.length < this.maxSize){
-            this.container.push(data)
-        }
+  push(data) {
+    if (this.container.length < this.maxSize) {
+      this.container.push(data);
     }
-    pop(){
-        this.container.pop()
-    }
+  }
+  pop() {
+    this.container.pop();
+  }
 }
-let newStack = new Stack()
-newStack.push('Page-01')
-newStack.push('Page-02')
-newStack.push('Page-03')
-newStack.push('Page-04')
-newStack.push('Page-05')
-newStack.pop()
+let newStack = new LIFOStack();
+newStack.push("Page-01");
+newStack.push("Page-02");
+newStack.push("Page-03");
+newStack.pop();
+newStack.push("Page-04");
+newStack.push("Page-05");
 
-console.log(newStack)
+console.log(newStack);
+
+// QUEUE
+// FIFO (First In - First Out)
+
+class FIFOStack {
+  maxSize = 3;
+  container = [];
+
+  push(data) {
+    if (this.container.length < this.maxSize) {
+      this.container.push(data);
+    }
+  }
+  shift() {
+    this.container.shift();
+  }
+}
+let newStack1 = new FIFOStack();
+newStack1.push("Page-01");
+newStack1.push("Page-02");
+newStack1.push("Page-03");
+newStack1.shift();
+newStack1.push("Page-04");
+newStack1.push("Page-05");
+
+console.log(newStack1);
+
+// SET
+const buah = ["Apel", "Anggur", "Mangga", "Apel", "Nangka"];
+const newBuah = new Set(buah);
+// console.log(newBuah)
+newBuah.add("Semangka");
+newBuah.add("Anggur");
+// console.log(newBuah)
+newBuah.delete("Anggur");
+// console.log(newBuah)
+console.log(newBuah.has("Apel")); // akan bersifat Boolean
+// newBuah.entries('Apel','Anggur')
+console.log(newBuah);
+
+const campus = ["BSD", "JKT", "BDG", "BTM"];
+const newCamp = new Set(campus);
+
+const campusEntries = newCamp.entries();
+
+let text = " ";
+for (const entry of campusEntries) {
+  text += entry;
+}
+console.log(text);
+
+newCamp.forEach((item) => {
+  console.log(item);
+});
+
+campus.forEach((index, item) => {
+  console.log(index);
+  console.log(item)
+});
